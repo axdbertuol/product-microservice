8f353f77595bcdd3db5ef4ee8ba2b6df87aaf177
axdbertuol <axdber@gmail.com>
2023-02-25 14:21:54 -0300

    feat: Adds product repository and interface

---
+++ src/products/repository/product.repository.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { Product, ProductDocument } from '../entities/product.entity'
+import { CreateProductDto } from '../dto/create-product.dto'
+import { RepositoryInterface } from '../types/repository'
+
+@Injectable()
+export class ProductRepository implements RepositoryInterface {
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async find(id: string): Promise<Product | null> {
+    return this.productModel.findById(id).exec()
+  }
+
+  async findAllByCategory(category: string): Promise<Product[]> {
+    return this.productModel.find({ 'category.$name': category }).exec()
+  }
+
+  async findAll(): Promise<Product[]> {
+    return this.productModel.find().exec()
+  }
+
+  async create(product: CreateProductDto): Promise<Product> {
+    const newProduct = new this.productModel(product)
+    return newProduct.save()
+  }
+
+  async update(id: string, product: Product): Promise<Product | null> {
+    return this.productModel
+      .findByIdAndUpdate(id, product, { new: true })
+      ?.exec()
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.productModel.findByIdAndDelete(id).exec()
+  }
+}
