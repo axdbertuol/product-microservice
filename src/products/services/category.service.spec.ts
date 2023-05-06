94737d0c94227b2e021e79ce0fa1e540e5247c90
axdbertuol <axdber@gmail.com>
2023-05-06 20:44:42 -0300

    test: (category-service)

--- src/products/services/category.service.spec.ts
+++ src/products/services/category.service.spec.ts
@@ -3,6 +3,8 @@ import { CategoryService } from './category.service'
 import { CategoryRepository } from '../repository/category.repository'
 import { Category } from '../entities/category.entity'
 import mongoose from 'mongoose'
+import { CreateCategoryDto } from '../dto/create-category.dto'
+import { UpdateCategoryDto } from '../dto/update-category.dto'
 
 describe('CategoryService', () => {
   let categoryService: CategoryService
@@ -79,6 +81,59 @@ describe('CategoryService', () => {
       const result = await categoryService.findAll()
 
       expect(result).toEqual(expectedCategories)
+    })
+  })
+  describe('create', () => {
+    it('should create a category', async () => {
+      const category: CreateCategoryDto = { name: 'Test Category' }
+      const createdCategory: Category = {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        _id: new mongoose.Types.ObjectId('60a0c618610a5a2a98d1975f'),
+        name: 'Test Category',
+      }
+      jest
+        .spyOn(categoryRepository, 'create')
+        .mockResolvedValue(createdCategory)
+
+      const result = await categoryService.create(category)
+      expect(result).toEqual(createdCategory)
+      expect(categoryRepository.create).toHaveBeenCalledWith(category)
+    })
+  })
+  describe('update', () => {
+    const mockCategory = new Category()
+    mockCategory.name = 'Mock Category'
+
+    it('should update a category', async () => {
+      const mockUpdateDto: UpdateCategoryDto = {
+        name: 'Updated Category',
+      }
+      const mockUpdatedCategory = new Category()
+      mockUpdatedCategory.name = 'Updated Category'
+
+      jest
+        .spyOn(categoryRepository, 'update')
+        .mockResolvedValue(mockUpdatedCategory)
+
+      const result = await categoryService.update('1', mockUpdateDto)
+
+      expect(result).toEqual(mockUpdatedCategory)
+    })
+
+    it('should return null if the category does not exist', async () => {
+      jest.spyOn(categoryRepository, 'update').mockResolvedValue(null)
+
+      const result = await categoryService.update('1', {} as UpdateCategoryDto)
+
+      expect(result).toBeNull()
+    })
+  })
+  describe('delete', () => {
+    it('should call CategoryRepository.delete with the given id', async () => {
+      const id = '60a54d933081fd5e4b6b34e2'
+      await categoryService.delete(id)
+      expect(categoryRepository.delete).toHaveBeenCalledWith(id)
     })
   })
 })
