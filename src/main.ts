84d90b1cad34fc51ecade166dafbce3da23bc9c3
axdbertuol <axdber@gmail.com>
2023-05-07 17:21:41 -0300

    feat: add log

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   await app.listen(3000)
+  console.log('Server listening on 3000')
 }
 bootstrap()
