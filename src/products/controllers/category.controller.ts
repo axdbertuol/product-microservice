31f7383bf737530267090c2e476391ace296f055
axdbertuol <axdber@gmail.com>
2023-05-16 21:35:41 -0300

    feat: update controller

--- src/products/controllers/category.controller.ts
+++ src/products/controllers/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Put, Delete, Param } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Delete,
+  Param,
+  ValidationPipe,
+} from '@nestjs/common'
 import { CRUD } from '../types/base.d'
 import { CategoryService } from '../services/category.service'
 import { Category } from '../entities/category.entity'
@@ -10,30 +19,32 @@ export class CategoryController implements CRUD {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Get(':id')
-  async find(@Param('id') id: string): Promise<Category | null> {
+  find(@Param('id') id: string): Promise<Category | null> {
     return this.categoryService.find(id)
   }
 
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() category: UpdateCategoryDto,
+  ): Promise<Category | null> {
+    const result = this.categoryService.update(id, category)
+    return result
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.categoryService.delete(id)
+  }
+
   @Get()
-  async findAll(): Promise<Category[]> {
+  findAll(): Promise<Category[]> {
     return this.categoryService.findAll()
   }
 
   @Post()
-  async create(@Body() category: CreateCategoryDto): Promise<Category> {
-    return this.categoryService.create(category)
-  }
-
-  @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() category: UpdateCategoryDto,
-  ): Promise<Category | null> {
-    return this.categoryService.update(id, category)
-  }
-
-  @Delete(':id')
-  async delete(@Param('id') id: string): Promise<void> {
-    return this.categoryService.delete(id)
+  create(@Body() category: CreateCategoryDto): Promise<Category> {
+    const result = this.categoryService.create(category)
+    return result
   }
 }
